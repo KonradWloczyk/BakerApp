704c9e47e55cbd6307f1f2ae55f061cd022ddbb3
Konrad Włoczyk <konrad_wloczyk@o2.pl>
2020-01-23 13:17:58 +0100

    Makeing first views and controllers

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BakerApp.Controllers
+{
+    public class ClientController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ Data/BakerAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BakerApp.Models;
+
+namespace BakerApp.Data
+{
+    public class BakerAppContext : DbContext
+    {
+        public BakerAppContext(DbContextOptions<BakerAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client{ get; set; }
+    }
+}

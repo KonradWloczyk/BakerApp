6a59b7f7ad40c48e8181101c2d94434ed00dbcdf
Konrad Włoczyk <konrad_wloczyk@o2.pl>
2020-01-27 13:04:57 +0100

    Add project files.

---
+++ Migrations/20200125145517_NewMigration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BakerApp.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Client");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Client");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Client");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Contact",
+                table: "Client",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Client",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Client",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "Client");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Client");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Client");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Client",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Client",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Client",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BakerApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string Region { get; set; }
+        public int Contact { get; set; }
+    }
+}
